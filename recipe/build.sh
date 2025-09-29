94885df9e3b1d2f94cacbc6cc49741ea8dd37d8a
Daniel Nachun <daniel.nachun@gmail.com>
2025-09-29 12:25:08 -0700

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,15 +9,25 @@ jq 'del(.scripts.prepare)' package.json.bak > package.json
 # Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
+    --no-bin-links \
     --global \
     --build-from-source \
     fsouza-${PKG_NAME}-${PKG_VERSION}.tgz
 
 # Create license report for dependencies
+mv package.json package.json.bak
+jq 'del(.packageManager)' package.json.bak > package.json
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
+mkdir -p ${PREFIX}/bin
+tee ${PREFIX}/bin/prettierd << EOF
+#!/bin/sh
+exec \${CONDA_PREFIX}/lib/node_modules/@fsouza/prettierd/bin/prettierd "\$@"
+EOF
+chmod +x ${PREFIX}/bin/prettierd
+
 # Create batch wrapper
 tee ${PREFIX}/bin/prettierd.cmd << EOF
-call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\prettierd %*
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\lib\node_modules\@fsouza\prettierd\bin\prettierd %*
 EOF
